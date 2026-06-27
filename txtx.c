640924a750b94d18fd77fccb5814b4c8bd5773b8
shreeRICHARD <shreerichard20@gmail.com>
2026-06-27 22:51:26 +0530

    summa

---
+++ txtx.c
@@ -0,0 +1,20 @@
+/*Tree Recursion */
+
+#include <stdio.h>
+
+ void function(int n){
+    if(n>0){
+        printf("%d",n);
+        function(n-1);
+        function(n-1);
+
+    }
+
+}
+void main(){
+
+    int x= 3;
+    function(x);
+   
+}
+
