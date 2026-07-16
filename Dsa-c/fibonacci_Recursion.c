35ddf17311391c7a9a08bd2305f40a0108a3f001
shreeRICHARD <shreerichard20@gmail.com>
2026-07-16 21:44:37 +0530

    fibonacci

--- Dsa-c/fibonacci_Recursion.c
+++ Dsa-c/fibonacci_Recursion.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-int fib(int n){
+int fib(int n)
+{
 
-    if(n<=0)
-    return n;
-    return fib(n-2) + fib(n-1);
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
 }
 
-int main(){
+int main()
+{
 
-    printf("%d",fib(8));
+    printf("%d", fib(5));
     return 0;
 }
